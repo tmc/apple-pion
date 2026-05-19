f019c66b6495e665a89de73cbc103ac93901f0fd
Travis Cline <travis.cline@gmail.com>
2026-05-18 22:08:44 -0700

    nwtransport: show pion setting engine setup

--- nwtransport/example_test.go
+++ nwtransport/example_test.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/pion/ice/v4"
+	"github.com/pion/webrtc/v4"
+	"github.com/tmc/apple-pion/icepolicy"
 	"github.com/tmc/apple-pion/nwtransport"
 	"github.com/tmc/apple/x/network/nwpacket"
 )
@@ -23,4 +26,38 @@ func ExampleNew() {
 
 	// Output:
 	// true true
+}
+
+func ExampleNew_pionSettingEngine() {
+	localIP := net.ParseIP("fe80::1")
+	netTransport, err := nwtransport.New(nwtransport.Config{
+		Packet: nwpacket.Config{
+			InterfaceName:     "awdl0",
+			LocalAddr:         &net.UDPAddr{IP: localIP},
+			IncludePeerToPeer: true,
+			RequireInterface:  true,
+		},
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var se webrtc.SettingEngine
+	se.SetNet(netTransport)
+	se.SetICEMulticastDNSMode(ice.MulticastDNSModeDisabled)
+	icepolicy.Policy{RawHostCandidates: true}.Configure(&se, ice.MulticastDNSModeDisabled, localIP)
+
+	api := webrtc.NewAPI(webrtc.WithSettingEngine(se))
+	pc, err := api.NewPeerConnection(webrtc.Configuration{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer pc.Close()
+
+	fmt.Println(true)
+
+	// Output:
+	// true
 }
